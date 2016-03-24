dd77b558306d11efbc07395e3e0eb50c83e11529
wuxiaofang <wuxiaofang@yongche.com>
2016-03-24 14:12:00 +0800

    refs #23468, first commit

---
+++ WXFCommonModule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXFCommonModule
+//
+//  Created by yongche_w on 16/3/24.
+//  Copyright © 2016年 yongche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXFCommonModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXFCommonModule
+//
+//  Created by yongche_w on 16/3/24.
+//  Copyright © 2016年 yongche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
